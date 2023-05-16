be0ef876a25039ca7af7e6834df68a5b77efaa78
Jarco-Uil <jarco@slimstampen.nl>
2023-05-16 14:24:13 +0200

    Run tests on gatling SSaaS

--- src/gatling/java/slimstampen/SlimStampenSimulation.java
+++ src/gatling/java/slimstampen/SlimStampenSimulation.java
@@ -6,17 +6,19 @@ import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
+import java.time.Duration;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class SlimStampenSimulation extends Simulation {
 
-    private final String baseUrl = "https://app-test.slimstampen.nl/ruggedlearning";
+    private final String baseUrl = "https://gatling.test.slimstampen.nl/ruggedlearning";
     private final int amountOfResponses = 100;
-    private final int amountOfUsers = 100;
+    private final int amountOfUsers = 200;
 
     FeederBuilder<String> userFeeder = csv("users.csv").circular();
-    FeederBuilder<Object> responseFeeder = jsonFile("responses.json").random();
+    FeederBuilder<Object> responseFeeder = jsonFile("gatling_responses_test.json").random();
 
     ChainBuilder jwks =
             exec(http("JWKS").get("/.well-known/jwks.json").check(status().is(200)));
@@ -27,18 +29,24 @@ public class SlimStampenSimulation extends Simulation {
                     .body(StringBody("{ \"username\": \"#{email}\", \"password\": \"#{password}\"}"))
                     .check(header("Set-Cookie").exists().saveAs("session"))
             )
-            .pause(1)
             .exec(addCookie(Cookie("Cookie", "#{session}")))
+            .pause(1)
             .exec(http("profile_get")
                     .get("/api/profile/get")
                     .check(bodyString().saveAs("body"))
                     .check(status().is(200))
-                    .check(jsonPath("$.anonymous").ofBoolean().is(false))
+                    .check(jsonPath("$.anonymous").ofBoolean().is(  false))
             )
+            .pause(2)
+            .exec(http("get_first_cue")
+                    .get("/api/response/getFirstCue/892")
+                    .check(bodyString().saveAs("first_cue"))
+                    .check(jsonPath("$.sessionId").saveAs("initialized_session_id"))
+                    .check(status().is(200)))
             .pause(2)
             .repeat(amountOfResponses).on(
                     feed(responseFeeder)
-                            .pause(2)
+                            .pause(session -> Duration.ofMillis(2000), session -> Duration.ofMillis(3000))
                             .exec(http("response_save")
                                     .post("/api/response/save")
                                     .body(StringBody("{\"alternatives\": \"[]\", " +
@@ -46,32 +54,31 @@ public class SlimStampenSimulation extends Simulation {
                                             "\"backSpaceUsed\": \"#{backspace_used}\", " +
                                             "\"backSpacedFirstLetter\": \"#{backspaced_first_letter}\", " +
                                             "\"correct\": \"#{correct}\", " +
-                                            "\"currenTime\": \"#{current_time}\", " +
                                             "\"factId\": \"#{fact_id}\", " +
                                             "\"givenResponse\": \"#{given_response}\", " +
-                                            "\"lessonId\": \"#{lesson_id}\", " +
-                                            "\"mostDifficult\": \"#{most_difficult}\", " +
-                                            "\"numberOfChoices\": \"#{number_of_choices}\", " +
+                                            "\"lessonId\": \"892\", " +
+                                            "\"mostDifficult\": \"false\", " +
                                             "\"presentationDuration\": \"#{presentation_duration}\", " +
                                             "\"presentationStartTime\": \"#{presentation_start_time}\", " +
                                             "\"presentedCueTextIndex\": \"#{presented_cue_text_index}\", " +
                                             "\"reactionTime\": \"#{reaction_time}\", " +
-                                            "\"sessionId\": \"#{session_id}\", " +
-                                            "\"sessionTime\": \"#{session_time}\"}"))
-                                    .check(status().is(200))
-                            )
-                            .exec(http("mastery_save")
-                                    .post("/api/mastery/save")
-                                    .body(StringBody("{ \"correct\": \"#{correct}\", " +
-                                            "\"factId\": \"#{fact_id}\", " +
-                                            "\"lessonId\": \"#{lesson_id}\", " +
-                                            "\"mostDifficult\": \"#{most_difficult}\", " +
-                                            "\"reactionTime\": \"#{reaction_time}\", " +
-                                            "\"sessionId\": \"#{session_id}\", " +
+                                            "\"sessionId\": \"#{initialized_session_id}\", " +
                                             "\"sessionTime\": \"#{session_time}\"}"))
                                     .check(status().is(200))
                             )
             );
+
+    ChainBuilder loadLibrary = feed(userFeeder)
+            .exec(http("login")
+                    .post("/api/user/login")
+                    .body(StringBody("{ \"username\": \"#{email}\", \"password\": \"#{password}\"}"))
+                    .check(header("Set-Cookie").exists().saveAs("session"))
+            )
+            .pause(1)
+            .exec(addCookie(Cookie("Cookie", "#{session}")))
+            .exec(http("load_lesson")
+                    .get("/api/lesson-group/library?languageTag=nl-NL&pageNo=0&pageSize=50&userIds=&searchTerm=&sortBy=TITLE&sortDirection=ASC")
+                    .check(status().is(200)));
 
     HttpProtocolBuilder httpProtocol =
             http.baseUrl(baseUrl)
@@ -83,13 +90,15 @@ public class SlimStampenSimulation extends Simulation {
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
                     );
 
-    ScenarioBuilder json = scenario("Json").exec(jwks);
-    ScenarioBuilder loginScenario = scenario("Login").exec(loginAndPractice);
+//    ScenarioBuilder json = scenario("Json").exec(jwks);
+    ScenarioBuilder loginScenario = scenario("Login and practice").exec(loginAndPractice);
+    ScenarioBuilder loadLibraryScenario = scenario("LoadLessons").exec(loadLibrary);
 
     {
         setUp(
-                json.injectOpen(rampUsers(amountOfUsers).during(10)),
-                loginScenario.injectOpen(rampUsers(amountOfUsers).during(30))
+//                json.injectOpen(rampUsers(amountOfUsers).during(50)),
+                loginScenario.injectOpen(rampUsers(amountOfUsers).during(50))
+//                loadLibraryScenario.injectOpen(rampUsers(amountOfUsers).during(20))
         ).protocols(httpProtocol);
     }
 }
