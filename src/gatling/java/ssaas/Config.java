675322d6dd22ac1f204a9eb279e147da2169cc8a
Jarco Uil <92713598+Jarco-Uil@users.noreply.github.com>
2025-11-12 16:58:38 +0100

    RL-19056 Add tests for API deadlock (#7)

---
+++ src/gatling/java/ssaas/Config.java
@@ -0,0 +1,16 @@
+package ssaas;
+
+import uttil.Environment;
+
+public class Config {
+    public static final Environment CURRENT_ENV = Environment.DEV;
+
+    public static String getBaseUrl() {
+        return switch (CURRENT_ENV) {
+            case DEV -> "http://localhost:8082/api";
+            case TEST -> "https://api.test.memorylab.app/api";
+            case STAGING -> "https://api.staging.memorylab.app/api";
+            default -> throw new IllegalStateException("Unknown environment");
+        };
+    }
+}
